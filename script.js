b1b04f9a46999a411ff907c5422b5a30644c6f41
Biplob <biplob.khadgi1@gmail.com>
2025-06-19 16:36:16 +0545

    adjust individual grid height to variable as well. Add reset functionso that the grid is reset while adjusting.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const container = document.querySelector("#container");
 const adjustGridBtn = document.querySelector("#adjustGridBtn");
 let gridSize = 16 //default;
-// let divs = []
+let divs = []
 
 adjustGridBtn.addEventListener("click", () => {
+    resetGrid();
     let gridSize = parseInt(prompt("Enter the size of the Grid: "));
     for(let i = 0; i<(gridSize*gridSize); i++){
         const div = document.createElement("div");
         div.style.width = `${640/gridSize}px`;
-        div.style.height = div.style.width;
+        div.style.height = `${640/gridSize}px`;
         div.addEventListener("mouseover", () => {
             div.style.backgroundColor = "yellow"
         }
         )
-        // divs.push(div);
+        divs.push(div);
         container.appendChild(div);
 }
 })
 
-// let resetGrid = () => {
-//     for(let i = (gridSize*gridSize); i>0; i--){
-//         container.removeChild(divs[i]);
-//     }
-// }
+const resetGrid = () => {
+  divs.forEach(div => container.removeChild(div));
+  divs = [];
+};
