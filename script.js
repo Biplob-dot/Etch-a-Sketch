22aca64ef695e4b2813fdae4d8d8da1807f1febd
Biplob <biplob.khadgi1@gmail.com>
2025-06-20 13:13:45 +0545

    Add Variable color as per the user prompt.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.querySelector("#container");
 const adjustGridBtn = document.querySelector("#adjustGridBtn");
-let gridSize = 16 //default;
-let divs = []
+let gridSize = 16; //default;
+let divs = [];
+let drawColor = "yellow" //default;
 
 adjustGridBtn.addEventListener("click", () => {
     resetGrid();
     let gridSize = parseInt(prompt("Enter the size of the Grid: "));
+    drawColor = prompt("Enter color: (red, blue, green, yellow)");
     if(gridSize > 100){
         alert("Enter less than 100!!");
     }
@@ -15,7 +17,7 @@ adjustGridBtn.addEventListener("click", () => {
             div.style.width = `${640/gridSize}px`;
             div.style.height = `${640/gridSize}px`;
             div.addEventListener("mouseover", () => {
-                div.style.backgroundColor = "yellow"
+                div.style.backgroundColor = drawColor;
             })
             divs.push(div);
             container.appendChild(div);
