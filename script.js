aa05f61265a2bfd4e173fe3e1cb33cbd7d9e7374
Biplob <biplob.khadgi1@gmail.com>
2025-06-20 14:03:09 +0545

    add grid clear button

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const container = document.querySelector("#container");
 const adjustGridBtn = document.querySelector("#adjustGridBtn");
+const clearGridBtn = document.querySelector("#clearGridBtn");
 let gridSize = 16; //default;
 let divs = [];
 let drawColor = "yellow" //default;
 
 adjustGridBtn.addEventListener("click", () => {
     resetGrid();
-    let gridSize = parseInt(prompt("Enter the size of the Grid: "));
-    drawColor = prompt("Enter color: (red, blue, green, yellow)");
+    gridSize = parseInt(prompt("Enter the size of the Grid: "));
     if(gridSize > 100){
         alert("Enter less than 100!!");
     }
-    else{
+    if(gridSize <= 100){
+        drawColor = prompt("Enter color: (red, blue, green, yellow)");
         for(let i = 0; i<(gridSize*gridSize); i++){
             const div = document.createElement("div");
             div.style.width = `${640/gridSize}px`;
@@ -29,3 +30,19 @@ const resetGrid = () => {
   divs.forEach(div => container.removeChild(div));
   divs = [];
 };
+
+const clearGrid = () => {
+    resetGrid();
+    for(let i = 0; i<(gridSize*gridSize); i++){
+            const div = document.createElement("div");
+            div.style.width = `${640/gridSize}px`;
+            div.style.height = `${640/gridSize}px`;
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = drawColor;
+            })
+            divs.push(div);
+            container.appendChild(div);
+        }
+}
+
+clearGridBtn.addEventListener("click", clearGrid);
