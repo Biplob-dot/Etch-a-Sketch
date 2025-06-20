d3f542296465af97ac080ba9bc73b174d864c0bc
Biplob <biplob.khadgi1@gmail.com>
2025-06-20 23:55:55 +0545

    add rgb values instead of name of colors Hence we can manipulate the opacity

--- script.js
+++ script.js
@@ -7,53 +7,69 @@ const greenBtn = document.querySelector("#greenBtn");
 const yellowBtn = document.querySelector("#yellowBtn");
 let gridSize = 16; //default;
 let divs = [];
-let drawColor = "white"; //default;
+let rgb_red = 255;
+let rgb_blue = 255;
+let rgb_green = 255;
+let drawColor = "rgb(255,255,255)"; //default;
 
 const makeGrid = () => {
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    const div = document.createElement("div");
-    div.style.width = `${640 / gridSize}px`;
-    div.style.height = `${640 / gridSize}px`;
-    div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = drawColor;
-    });
-    divs.push(div);
-    canvasContainer.appendChild(div);
-  }
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const div = document.createElement("div");
+        div.style.width = `${640 / gridSize}px`;
+        div.style.height = `${640 / gridSize}px`;
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = `rgb(${rgb_red},${rgb_green},${rgb_blue})`;
+            // const index = divs.indexOf(div);
+            // if (index > 0) {
+            //     const previousDiv = divs[index - 1];
+            //     previousDiv.style.backgroundColor = "lightgray"; // or any color/action
+            // }
+        });
+        divs.push(div);
+        canvasContainer.appendChild(div);
+    }
 };
 
 adjustGridBtn.addEventListener("click", () => {
-  resetGrid();
-  gridSize = parseInt(prompt("Enter the size of the Grid: "));
-  if (gridSize > 100) {
-    alert("Enter less than 100!!");
-  }
-  if (gridSize <= 100) {
-    makeGrid();
-  }
+    resetGrid();
+    gridSize = parseInt(prompt("Enter the size of the Grid: "));
+    if (gridSize > 100) {
+        alert("Enter less than 100!!");
+    }
+    if (gridSize <= 100) {
+        makeGrid();
+    }
 });
 
 const resetGrid = () => {
-  divs.forEach((div) => canvasContainer.removeChild(div));
-  divs = [];
+    divs.forEach((div) => canvasContainer.removeChild(div));
+    divs = [];
 };
 
 const clearGrid = () => {
-  resetGrid();
-  makeGrid();
+    resetGrid();
+    makeGrid();
 };
 
 clearGridBtn.addEventListener("click", clearGrid);
 
 redBtn.addEventListener("click", () => {
-  drawColor = 'red';
+    rgb_red = 255;
+    rgb_blue = 0;
+    rgb_green = 0;
 });
 blueBtn.addEventListener("click", () => {
-  drawColor = 'blue';
+    rgb_red = 0;
+    rgb_blue = 255;
+    rgb_green = 0;
 });
 greenBtn.addEventListener("click", () => {
-  drawColor = 'green';
+    rgb_red = 0;
+    rgb_blue = 0;
+    rgb_green = 255;
 });
 yellowBtn.addEventListener("click", () => {
-  drawColor = 'yellow';
+    rgb_red = 255;
+    rgb_blue = 0;
+    rgb_green = 255;
 });
